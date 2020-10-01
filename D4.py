a7f7948d52785f9a4bab162d1733470dbd96cc97
Sriashika Addala <49125522+ashika29@users.noreply.github.com>
2020-10-01 16:16:12 +0530

    Create D4.py

---
+++ D4.py
@@ -0,0 +1,20 @@
+// Question link: https://www.hackerrank.com/challenges/30-class-vs-instance/problem
+// Code section:
+class Person:
+    def __init__(self,initialAge):
+        if initialAge < 0:
+            initialAge = 0
+            print("Age is not valid, setting age to 0.")
+        else:
+            pass
+        self.age = initialAge
+    def amIOld(self):
+        if self.age < 13:
+            print("You are young.")
+        elif self.age >= 13 and self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+    def yearPasses(self):
+        self.age += 1
+
