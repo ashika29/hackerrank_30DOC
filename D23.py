6138691182ff888268e175cc685041829da9d60a
Sriashika Addala <49125522+ashika29@users.noreply.github.com>
2020-10-01 16:42:17 +0530

    Create D23.py

---
+++ D23.py
@@ -0,0 +1,40 @@
+# Question link: https://www.hackerrank.com/challenges/30-binary-trees/problem
+# Code:
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+    def levelOrder(self,root):
+        if root is None:
+            return
+        queue =[]
+        queue.append(root)
+        while(len(queue) > 0):
+            print queue[0].data,
+            node = queue.pop(0)
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+
+T=int(raw_input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(raw_input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
