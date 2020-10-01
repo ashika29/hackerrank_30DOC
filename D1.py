afaeafe18e4817d2ab3239d5faf9557bf2096bf2
Sriashika Addala <49125522+ashika29@users.noreply.github.com>
2020-10-01 16:12:58 +0530

    Uploading Day 1 Code

---
+++ D1.py
@@ -0,0 +1,15 @@
+'''
+Question link:
+https://www.hackerrank.com/challenges/30-data-types/problem
+'''
+
+#Code section:
+i = 4
+d = 4.0
+s = 'HackerRank '
+xi = int(input())
+xd = float(input())
+xs = str(input())
+print(xi+i)
+print(xd+d)
+print(s + xs)
