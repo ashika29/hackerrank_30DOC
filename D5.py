58bdce14ae9df520a24da4262f72371efc076950
Sriashika Addala <49125522+ashika29@users.noreply.github.com>
2020-10-01 16:17:01 +0530

    Create D5.py

---
+++ D5.py
@@ -0,0 +1,8 @@
+// Question link: https://www.hackerrank.com/challenges/30-loops/problem
+// Code section:
+#!/bin/python3
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        print("%d x %d = %d"%(n,i,(n*i)))
+
